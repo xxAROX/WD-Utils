ab41730695f6d84fe6587579eeef66e01e8f7831
xxAROX <arox.email.0@gmail.com>
2023-05-07 21:51:53 +0200

    Added dispatching for downstream/proxy commands for both sides

---
+++ docs/ScriptCustomEventPacket.md
@@ -0,0 +1,16 @@
+# ScriptCustomEventPacket
+
+<p style="text-align: center;">
+
+|            Event            	| Data                     	|
+|:---------------------------:	|--------------------------	|
+| waterdogpe:dispatch_command 	| string                   	|
+
+</p>
+
+<details>
+<summary>dispatch_command:</summary>
+
+> This is used to execute proxy-side commands as the player from dwonstrea-server-side (API)<br>
+> *Can be used in both directions **(see in [DownstreamCustomEventHandleScript.php](../DownstreamCustomEventHandleScript.php))***
+</details>
--- src/main/java/xxAROX/WDUtils/WDUtilsPlugin.java
+++ src/main/java/xxAROX/WDUtils/WDUtilsPlugin.java
@@ -1,17 +1,22 @@
 package xxAROX.WDUtils;
 
+import dev.waterdog.waterdogpe.event.defaults.PlayerLoginEvent;
+import dev.waterdog.waterdogpe.network.PacketDirection;
 import dev.waterdog.waterdogpe.network.protocol.ProtocolCodecs;
 import dev.waterdog.waterdogpe.plugin.Plugin;
 import org.cloudburstmc.protocol.bedrock.codec.BedrockPacketDefinition;
+import org.cloudburstmc.protocol.bedrock.packet.ScriptCustomEventPacket;
 import org.cloudburstmc.protocol.bedrock.packet.UpdateSoftEnumPacket;
+import org.cloudburstmc.protocol.common.PacketSignal;
 import xxAROX.WDUtils.commands.PluginsCommand;
 import xxAROX.WDUtils.commands.ReloadCommand;
 
 public class WDUtilsPlugin extends Plugin {
+    public static final String IDENTIFIER = "waterdogpe:";
 
     @Override
     public void onStartup() {
-        getDataFolder().delete();
+        saveResource("config.yml");
     }
 
     @Override
@@ -21,7 +26,25 @@ public class WDUtilsPlugin extends Plugin {
         ProtocolCodecs.addUpdater((builder, bedrockCodec) -> {
             BedrockPacketDefinition<UpdateSoftEnumPacket> updateSoftEnumPacketBedrockPacketDefinition = bedrockCodec.getPacketDefinition(UpdateSoftEnumPacket.class);
             builder.registerPacket(UpdateSoftEnumPacket::new, updateSoftEnumPacketBedrockPacketDefinition.getSerializer(), updateSoftEnumPacketBedrockPacketDefinition.getId());
+
+            if (getConfig().getBoolean("enable-script-event-actions", false)) {
+                BedrockPacketDefinition<ScriptCustomEventPacket> scriptCustomEventPacketBedrockPacketDefinition = bedrockCodec.getPacketDefinition(ScriptCustomEventPacket.class);
+                builder.registerPacket(ScriptCustomEventPacket::new, scriptCustomEventPacketBedrockPacketDefinition.getSerializer(), scriptCustomEventPacketBedrockPacketDefinition.getId());
+            }
             return builder;
         });
+        if (getConfig().getBoolean("enable-script-event-actions", false)) {
+            getProxy().getEventManager().subscribe(PlayerLoginEvent.class, event -> event.getPlayer().getPluginPacketHandlers().add((bedrockPacket, direction) -> {
+                if (direction.equals(PacketDirection.FROM_SERVER) && bedrockPacket instanceof ScriptCustomEventPacket packet) {
+                    switch (packet.getEventName().toLowerCase().replace(IDENTIFIER, "")) {
+                        case "dispatch_command": {
+                            getProxy().dispatchCommand(event.getPlayer(), packet.getData());
+                            return PacketSignal.HANDLED;
+                        }
+                    }
+                }
+                return PacketSignal.UNHANDLED;
+            }));
+        }
     }
 }
