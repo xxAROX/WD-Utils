7323d4a513dbe6a446a5469d3205103ef65ee80e
xxAROX <arox.email.0@gmail.com>
2023-06-07 17:35:42 +0200

    working!
    Signed-off-by: xxAROX <arox.email.0@gmail.com>

--- src/main/java/xxAROX/WDUtils/event/lang/LanguagesLoadEvent.java
+++ src/main/java/xxAROX/WDUtils/event/lang/LanguagesLoadEvent.java
@@ -1,17 +1,14 @@
 package xxAROX.WDUtils.event.lang;
 
 import dev.waterdog.waterdogpe.event.Event;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 import xxAROX.WDUtils.lang.Language;
 
 import java.util.HashMap;
 
-@Getter @Setter
+@Getter @Setter @AllArgsConstructor
 public class LanguagesLoadEvent extends Event {
     private HashMap<String, Language> languages;
-
-    public LanguagesLoadEvent(HashMap<String, Language> languages) {
-        this.languages = languages;
-    }
 }
--- src/main/java/xxAROX/WDUtils/lang/Language.java
+++ src/main/java/xxAROX/WDUtils/lang/Language.java
@@ -3,14 +3,15 @@ package xxAROX.WDUtils.lang;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import jline.internal.Nullable;
+import lombok.Getter;
 import lombok.NonNull;
 
 import java.util.HashMap;
 import java.util.Map;
 
 public final class Language {
-    private final String locale;
-    private final String name;
+    @Getter private final String locale;
+    @Getter private final String name;
     private final HashMap<String, String> translations = new HashMap<>();
 
     public Language(@NonNull String locale, @NonNull JsonObject json) {
@@ -22,13 +23,5 @@ public final class Language {
     @Nullable
     public String getTranslation(String key){
         return translations.getOrDefault(key, null);
-    }
-
-    public String getLocale() {
-        return locale;
-    }
-
-    public String getName() {
-        return name;
     }
 }
